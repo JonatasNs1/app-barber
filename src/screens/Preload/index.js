5ef254729a44cda0a70b96262beca037345251e4
jonatasNs1 <jonatassantos152409@gmail.com>
2023-04-10 18:03:10 -0300

    estilizando a tela de login

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
-// import { Text } from 'react-native';
-
+import React, { useEffect } from 'react';
 import {Container, LoadingIcon} from './styles';
-
 import BarberLogo from '../../assets/barber.svg';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useNavigation} from '@react-navigation/native'
+
+
 
 export default () =>{
+
+    const navigation = useNavigation();
+
+    useEffect(() =>{
+        const checkToken = async ()=>{
+            const token = await AsyncStorage.getItem('token');
+            if(token !== null){
+                //validar o token
+            }
+            else{
+                navigation.navigate('SignIn');
+            }
+        }
+        checkToken();
+
+    },[]);
+
     return(
         <Container>
             <BarberLogo width="100%" height="160"/>
